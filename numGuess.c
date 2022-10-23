9da8027d37d3ca6bcabebe32990e4d6de5e945d8
wfgarcia <wfgarcia@cpp.edu>
2022-10-22 19:50:29 -0700

    outline

---
+++ numGuess.c
@@ -0,0 +1,21 @@
+#include <stdio.c>
+
+void game(){
+    //picks number
+
+    //checks number
+
+    //if number is less than or greater tha
+}
+
+void main(){
+    //main menu
+
+    //decides what to do
+
+    //places number
+
+    //quits program
+
+
+}
