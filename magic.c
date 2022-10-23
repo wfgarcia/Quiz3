523388fa6de7e19ff4e5abc385044779948c580a
wfgarcia <wfgarcia@cpp.edu>
2022-10-23 08:55:39 -0700

    inital variables

---
+++ magic.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int square[3][3]= {
+    {0, 0, 0},
+    {0, 0, 0},
+    {0, 0, 0}
+};
+
+int numbers[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+int main(void){
+    srand(time(NULL));
+
+    // declare array
+    int puzzle[][3] = {
+        {4, 9, 2},
+        {3, 5, 7},
+        {8, 1, 6}
+    };
+
+    int found = 0;
+    int count = 0;
+    // create board 
+
+
+    //create 2D array 
+    
+    //random board
+
+    // test board if equal to 15
+
+    //list outcome
+
+
+}
