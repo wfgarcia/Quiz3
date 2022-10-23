0c52aec3aa4281260903f1d6b19dfce185dda8eb
wfgarcia <wfgarcia@cpp.edu>
2022-10-23 12:47:07 -0700

    added salary search

--- EmployeeTwo.c
+++ EmployeeTwo.c
@@ -56,6 +56,16 @@ static int compareEmployeeName(const void *targetPtr, PtrToConstEmployee tableVa
 
 } 
 
+static int compareEmployeePhone(const void *targetPtr, PtrToConstEmployee tableValuePtr)
+{
+    return strcmp((char *) targetPtr, tableValuePtr->name);
+}
+
+static int compareEmployeeSalary(const void *targetPtr, PtrToConstEmployee tableValuePtr)
+{
+    return * (long *) targetPtr != tableValuePtr->salary;
+}
+
  
 
  
@@ -79,3 +89,13 @@ PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int size, char* name)
     return searchEmployeeTable(ptr, size, name, compareEmployeeName); 
 
 } 
+
+PtrToEmployee searchEmployeeByPhone(PtrToConstEmployee ptr, int size, char* phone)
+{
+    return searchEmployeeTable(ptr, size, phone, compareEmployeePhone);
+}
+
+PtrToEmployee searchEmployeeSalary(PtrToConstEmployee ptr, int size, double salary)
+{
+    return searchEmployeeTable(ptr, size, &salary, compareEmployeeSalary);
+}
