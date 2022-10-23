3402f28ab065c5e1f97b85d93a78d8985ba44975
wfgarcia <wfgarcia@cpp.edu>
2022-10-23 09:08:43 -0700

    random number added

--- magic.c
+++ magic.c
@@ -8,8 +8,36 @@ int square[3][3]= {
     {0, 0, 0}
 };
 
-int numbers[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+int values[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+// check inputs 
 
+
+
+
+
+// print final array
+
+
+
+
+// change values 
+void random(){
+    for(int i = sizeof(values); i > 0; i--){
+        int r = (rand() % (i + 1));
+        int temp = values[i];
+        values[i] = values[r];
+        values[r] = temp;
+    }
+}
+
+
+
+
+//generates random grids
+void createGrid(){
+    random();
+    int n = 0;
+}
 int main(void){
     srand(time(NULL));
 
@@ -22,16 +50,14 @@ int main(void){
 
     int found = 0;
     int count = 0;
-    // create board 
 
 
-    //create 2D array 
-    
-    //random board
+    // loop number of times it takes
+    while(found == 0){
+        
+    }
 
-    // test board if equal to 15
 
-    //list outcome
 
 
 }
